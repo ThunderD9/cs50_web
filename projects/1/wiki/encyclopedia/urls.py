969d4fbbae764a64bd1a7622e32453d1a52060a5
ThunderD9 <thunderd924@gmail.com>
2022-08-28 12:43:00 +0530

    Edit Button is done

--- projects/1/wiki/encyclopedia/urls.py
+++ projects/1/wiki/encyclopedia/urls.py
@@ -1,7 +1,15 @@
+
 from django.urls import path
 
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("wiki/<str:title>", views.entry, name="entry"),
+    path("search/", views.search, name="search"),
+    path("newpage/", views.new_page, name="newpage"),
+    path("randompage/", views.random_page, name = "randompage"),
+    path("edit/", views.edit, name="edit"),
+    path("save_edit", views.save_edit, name="save_edit")
 ]
+ 
--- projects/1/wiki/encyclopedia/views.py
+++ projects/1/wiki/encyclopedia/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
 
+
+from django.shortcuts import render
+import markdown2
 from . import util
+import random
 
 
 def index(request):
@@ -8,3 +11,68 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def md_to_html(title):
+    page = util.get_entry(title)
+    markdowner = markdown2.Markdown()
+    if page == None:
+        return None
+    else:
+        return markdowner.convert(page)
+
+def entry(request, title):
+    content_html = md_to_html(title)
+    if content_html == None:
+        return render(request, "encyclopedia/error.html",{
+            "message":"This page does not exist"
+        })
+    return render(request, "encyclopedia/entry.html", {
+        "title" : title,
+        "content": content_html
+    })
+
+def search(request):
+    if request.method == "POST":
+        item_searched = request.POST['q']
+        content_html = md_to_html(item_searched)
+        if content_html != None:
+            return entry(request, item_searched)
+        else:
+            all_entries = util.list_entries()
+            elements = []
+            for item in all_entries:
+                if item_searched.lower() in item.lower():
+                    elements.append(item)
+            return render(request, "encyclopedia/search.html",{
+                "list":elements
+            })
+def random_page(request):
+    rand_choice= random.choice(util.list_entries())
+    return entry(request, rand_choice)
+
+def new_page(request):
+    if request.method == "GET":
+        return render(request, "encyclopedia/new_page.html")
+    elif request.method == "POST":
+        title = request.POST["title"]
+        content = request.POST["content"]
+        exists = util.get_entry(title)
+        if exists is not None:
+            return render(request, "encyclopedia/error.html",{
+            "message":"This page already exists"
+        })
+        else:
+            util.save_entry(title, content)
+            return entry(request, title)  
+
+
+def edit(request):
+    if request.method == "POST":
+        title = request.POST["entry_title"]
+        content = util.get_entry(title)
+        return render(request, "encyclopedia/edit.html",{
+            "title":title,
+            "content": content
+        })
+
+def save_edit(request):
+    return
