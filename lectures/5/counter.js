d3f1318f6339cb0afb86a0e783738d76cf505bf7
ThunderD9 <thunderd924@gmail.com>
2024-07-17 01:26:28 +0530

    adding 5th lecture

--- lectures/5/counter.js
+++ lectures/5/counter.js
@@ -1,14 +1,28 @@
-let counter = 0;
+
+
+if (!localStorage.getItem('counter')){
+    localStorage.setItem('counter', 0)
+}
+function z(){
+    document.querySelector('h1').innerHTML = 0;
+    localStorage.setItem('counter', 0)
+}
 function count(){
-    counter +=1;
+    let counter = localStorage.getItem('counter');
+    counter++ ;
     const heading = document.querySelector('h1');
     heading.innerHTML = counter;
-
-    if (counter % 10 === 0){
-        alert(`Count is now ${counter}`);
-    }
-
+    localStorage.setItem('counter', counter)
 }
-document.addEventListener('DOMContentLoaded', function(){
+
+
+
+// For event listening
+document.addEventListener('DOMContentLoaded', function(){ 
+    //loads all the page and runs so we can avoid the inability to run written in the bottom lines
+    document.querySelector('h1').innerHTML = localStorage.getItem('counter');
     document.querySelector('button').onclick = count;
-})
+    document.querySelector('#zero').onclick = z;
+
+    setInterval(count, 100); //run count function for every one second
+});
